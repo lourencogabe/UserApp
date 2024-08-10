5b8fabeba873c1df8398f0effb1fabe97d5ee539
Gabriel Lourenço <gabriels211@hotmail.com>
2024-08-10 16:48:27 -0300

    Initial Commit

---
+++ UserApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserApp
+//
+//  Created by Gabriel Lourenço on 10/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserApp/UserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAppApp.swift
+//  UserApp
+//
+//  Created by Gabriel Lourenço on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct UserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
