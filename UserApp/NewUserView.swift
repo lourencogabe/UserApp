eb025c92865942bb449632d6e45f426128878798
lourencogabe <gabriels211@hotmail.com>
2024-08-10 20:35:02 -0300

    :building_construction: feat: add views for list and new user

---
+++ UserApp/NewUserView.swift
@@ -0,0 +1,18 @@
+//
+//  NewUserView.swift
+//  UserApp
+//
+//  Created by Gabriel Lourenço on 10/08/24.
+//
+
+import SwiftUI
+
+struct NewUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewUserView()
+}
