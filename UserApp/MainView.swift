7051a9ea6426747d53e7b8d2eb3439382df30b2a
lourencogabe <gabriels211@hotmail.com>
2024-08-10 22:42:33 -0300

    :building_construction: feat: update menu

--- UserApp/MainView.swift
+++ UserApp/MainView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                NavigationLink(destination: NewUserView()){
+                    Text("Novo Usuário")
+                    .padding()
+                    .background(.green)
+                    .foregroundStyle(.white)
+                    .cornerRadius(8)
+                }
+                .padding()
+                
+                NavigationLink( destination: UserListView()){
+                    Text("Lista de Usuários")
+                    .padding()
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .cornerRadius(8)
+                }
+                .padding()
+            }
+            .navigationTitle("Menu Principal")
+        }
     }
 }
 
