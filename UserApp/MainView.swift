ff4a1e271fe413dd6dc22e0a81bdb87ba361a2df
lourencogabe <gabriels211@hotmail.com>
2024-08-10 22:39:20 -0300

    :building_construction: feat: create menu

---
+++ UserApp/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  UserApp
+//
+//  Created by Gabriel Lourenço on 10/08/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
